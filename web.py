92fea6d42f2a79babc8fcdea297ea61ae3fc697e
su-kaka <3493227712@qq.com>
2026-01-09 00:24:16 +0800

    Update web.py

--- web.py
+++ web.py
@@ -161,7 +161,7 @@ async def main():
     log.info(f"   Geminicli (Gemini格式): http://127.0.0.1:{port}")
     
     log.info(f"   Antigravity (OpenAI格式): http://127.0.0.1:{port}/antigravity/v1")
-    log.info(f"   Antigravity (claude格式): http://127.0.0.1:{port}/antigravity/v1")
+    log.info(f"   Antigravity (Claude格式): http://127.0.0.1:{port}/antigravity/v1")
     log.info(f"   Antigravity (Gemini格式): http://127.0.0.1:{port}/antigravity")
 
     # 配置hypercorn
