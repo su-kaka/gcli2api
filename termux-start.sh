5df0e2187ab3946a8b2b9daaaf8053d236c8902d
su-kaka <3493227712@qq.com>
2026-03-05 19:36:05 +0800

    Update termux-start.sh

--- termux-start.sh
+++ termux-start.sh
@@ -2,4 +2,4 @@ echo "强制同步项目代码，忽略本地修改..."
 git fetch --all
 git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
 pip install -r requirements-termux.txt
-pm2 start python --name web -- web.py
+pm2 start python --name gcli2api -- web.py
