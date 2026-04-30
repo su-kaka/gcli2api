1d60d9ba9fe9baa4a3be902fa3342e2856eafa9a
su-kaka <3493227712@qq.com>
2026-04-30 08:23:06 +0800

    Revert "优化反重力503报错"
    This reverts commit e488d1a1945ec64d4b5c2941487b8e30c9f7260c.

--- front/common.js
+++ front/common.js
@@ -2743,6 +2743,7 @@ function populateConfigForm() {
     setConfigField('googleapisProxyUrl', c.googleapis_proxy_url || '');
     setConfigField('resourceManagerApiUrl', c.resource_manager_api_url || '');
     setConfigField('serviceUsageApiUrl', c.service_usage_api_url || '');
+    setConfigField('antigravityApiUrl', c.antigravity_api_url || '');
 
     document.getElementById('autoBanEnabled').checked = Boolean(c.auto_ban_enabled);
     setConfigField('autoBanErrorCodes', (c.auto_ban_error_codes || []).join(','));
@@ -2797,6 +2798,7 @@ async function saveConfig() {
             googleapis_proxy_url: getValue('googleapisProxyUrl'),
             resource_manager_api_url: getValue('resourceManagerApiUrl'),
             service_usage_api_url: getValue('serviceUsageApiUrl'),
+            antigravity_api_url: getValue('antigravityApiUrl'),
             auto_ban_enabled: getChecked('autoBanEnabled'),
             auto_ban_error_codes: getValue('autoBanErrorCodes').split(',')
                 .map(c => parseInt(c.trim())).filter(c => !isNaN(c)),
@@ -2849,7 +2851,8 @@ const mirrorUrls = {
     oauthProxyUrl: 'https://gcli-api.sukaka.top/oauth2',
     googleapisProxyUrl: 'https://gcli-api.sukaka.top/googleapis',
     resourceManagerApiUrl: 'https://gcli-api.sukaka.top/cloudresourcemanager',
-    serviceUsageApiUrl: 'https://gcli-api.sukaka.top/serviceusage'
+    serviceUsageApiUrl: 'https://gcli-api.sukaka.top/serviceusage',
+    antigravityApiUrl: 'https://gcli-api.sukaka.top/daily-cloudcode-pa'
 };
 
 const officialUrls = {
@@ -2857,7 +2860,8 @@ const officialUrls = {
     oauthProxyUrl: 'https://oauth2.googleapis.com',
     googleapisProxyUrl: 'https://www.googleapis.com',
     resourceManagerApiUrl: 'https://cloudresourcemanager.googleapis.com',
-    serviceUsageApiUrl: 'https://serviceusage.googleapis.com'
+    serviceUsageApiUrl: 'https://serviceusage.googleapis.com',
+    antigravityApiUrl: 'https://daily-cloudcode-pa.sandbox.googleapis.com'
 };
 
 function useMirrorUrls() {
